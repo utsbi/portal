5f744116ec41d814f480b0ae3d0450c9416781c4
Wavefire5201 <70147423+Wavefire5201@users.noreply.github.com>
2025-08-05 00:48:21 -0500

    feat(frontend): move over contact and outreach page

---
+++ frontend/app/(static)/contact/page.tsx
@@ -0,0 +1,85 @@
+export default function Contact() {
+	return (
+		<section className="px-6 py-10 pt-36 md:px-32 2xl:px-80 relative font-OldStandardTT">
+			<div className="text-4xl mb-6">Contact Us</div>
+			<div className="text-xl mb-10">
+				Have a question or want to collaborate with us on a project? Fill out
+				the form below and we'll get back to you.
+			</div>
+
+			{/* TODO: Post to discord webhook*/}
+			<div className="max-w-2xl mx-auto">
+				<form name="contact" method="POST" data-netlify="true">
+					<div className="flex flex-col md:flex-row gap-6 mb-6">
+						<div className="form-group flex-1 w-full max-w-full">
+							<label htmlFor="name" className="block text-lg mb-2">
+								Name
+							</label>
+							<input
+								type="text"
+								name="name"
+								id="name"
+								required
+								className="w-full px-4 py-3 border-2 border-solid border-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 box-border transition duration-200"
+							/>
+						</div>
+
+						<div className="form-group flex-1 w-full max-w-full">
+							<label htmlFor="email" className="block text-lg mb-2">
+								Email
+							</label>
+							<input
+								type="email"
+								name="email"
+								id="email"
+								required
+								className="w-full px-4 py-3 border-2 border-solid border-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 box-border transition duration-200"
+							/>
+						</div>
+					</div>
+
+					<div className="form-group w-full max-w-full">
+						<label htmlFor="message" className="block text-lg mb-2">
+							Message
+						</label>
+						<textarea
+							name="message"
+							id="message"
+							rows={6}
+							required
+							className="w-full px-4 py-3 border-2 border-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 box-border transition duration-200"
+						></textarea>
+					</div>
+
+					<div className="flex justify-start mt-6">
+						<button
+							type="submit"
+							className="bg-green-500 hover:bg-green-600 text-white transition duration-300 pt-3 pb-2 px-6 border border-green-500 hover:border-green-500 rounded"
+						>
+							Send Message
+						</button>
+					</div>
+				</form>
+				<div className="mt-6 text-xl">
+					<div>Prefer to reach out directly?</div>
+					<div>
+						Contact our presidents:
+						<a
+							href="mailto:pedro@utsbi.org"
+							className="text-green-500 hover:underline font-semibold"
+						>
+							Pedro Guzman
+						</a>{" "}
+						or
+						<a
+							href="mailto:sam@utsbi.org"
+							className="text-green-500 hover:underline font-semibold"
+						>
+							Sam Moran
+						</a>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
---
+++ frontend/app/(static)/outreach/page.tsx
@@ -0,0 +1,24 @@
+export default function Outreach() {
+	return (
+		<>
+			<section className="px-6 py-10 pt-36 md:px-32 2xl:px-80 relative font-OldStandardTT">
+				<div className="text-4xl">Outreach</div>
+				<div className="text-xl pt-10">
+					Explore our growing network of high schools and colleges, showcasing
+					our current connections and future outreach goals to expand
+					sustainable impact.
+				</div>
+			</section>
+			<section className="px-6 pb-10 md:px-32 2xl:px-80 relative font-OldStandardTT">
+				<div className="relative" style={{ paddingTop: "56.25%" }}>
+					<iframe
+						src="https://www.google.com/maps/d/embed?mid=1ItBSYv-_H_PJBScpnU8gLMzr9F4qA6Q&ehbc=2E312F"
+						className="absolute top-0 left-0 w-full h-full"
+						allowFullScreen
+						title="Outreach Map"
+					></iframe>
+				</div>
+			</section>
+		</>
+	);
+}
