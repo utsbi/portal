34fdf463e5775b6ec0303ef15271a7e3dd1ad6f6
Wavefire5201 <70147423+Wavefire5201@users.noreply.github.com>
2025-08-15 01:25:38 -0500

    feat(frontend): add animation to login page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Container, Flex } from "@mantine/core";
+import { motion } from "framer-motion";
 import bg from "@/assets/images/login.jpg";
 import { Authentication } from "./Authentication";
 
@@ -15,16 +16,22 @@ export default function Login() {
 				backgroundRepeat: "no-repeat",
 			}}
 		>
-			<Container size="sm" style={{ minHeight: "100vh" }}>
-				<Flex
-					direction="column"
-					justify="center"
-					align="center"
-					style={{ minHeight: "100vh" }}
-				>
-					<Authentication />
-				</Flex>
-			</Container>
+			<motion.div
+				initial={{ opacity: 0, y: 20 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{ duration: 0.7 }}
+			>
+				<Container size="sm" style={{ minHeight: "100vh" }}>
+					<Flex
+						direction="column"
+						justify="center"
+						align="center"
+						style={{ minHeight: "100vh" }}
+					>
+						<Authentication />
+					</Flex>
+				</Container>
+			</motion.div>
 		</div>
 	);
 }
