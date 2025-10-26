80257795c3d3f3221b3a4753932f85be1bef0c42
Wavefire5201 <70147423+Wavefire5201@users.noreply.github.com>
2025-10-25 21:37:48 -0500

    feat: add custom 404 page

---
+++ frontend/app/not-found.js
@@ -0,0 +1,7 @@
+export default function Custom404() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center text-2xl">
+      <h1>This page does not exist :(</h1>
+    </div>
+  );
+}
