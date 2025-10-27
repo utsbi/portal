bc7b7ce7c53f80b1e18883cd00a3800fca9e9a4f
Wavefire5201 <70147423+Wavefire5201@users.noreply.github.com>
2025-10-27 17:28:26 -0500

    ci(frontend): fix layout components and enhance 404 page with Navbar and Footer

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -1,7 +1,17 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
 export default function Custom404() {
-  return (
-    <div className="w-full h-screen flex justify-center items-center text-2xl">
-      <h1>This page does not exist :(</h1>
-    </div>
-  );
+	return (
+		<>
+			<Navbar />
+			<div className="w-full h-screen flex justify-center items-center text-2xl">
+				<div className="flex flex-col justify-center items-center gap-2">
+					<div className="text-9xl">404</div>
+					<h1>This page does not exist :(</h1>
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
 }
