ff425a3f0ba284846b8296e57bde1f630088cf66
bhoang8578 <87407967+bhoang8578@users.noreply.github.com>
2025-12-30 01:55:27 -0600

    design of file structure and set up sorting functions

---
+++ frontend/app/(dashboard)/dashboard/lifecycle/[projectId]/page.tsx
@@ -0,0 +1,4 @@
+//project detail page or task list
+import { Project, Task } from '../types';
+import { sortTasks } from '../utils';
+import TaskCard from '../components/TaskCard';
--- frontend/app/(dashboard)/dashboard/lifecycle/page.tsx
+++ frontend/app/(dashboard)/dashboard/lifecycle/page.tsx
@@ -1,5 +1,9 @@
 import { redirect } from 'next/navigation';
 
+import { Project } from './types';
+import { sortProjects } from './utils';
+import ProjectCard from './components/ProjectCard';
+
 export default function LifecyclePage() {
   redirect('/error');
 }
